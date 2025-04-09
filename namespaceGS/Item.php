9537c5469a4788ff0062d902305af67000032232
notSinhaSid <deadlysinhasiddhartha2000@gmail.com>
2025-04-09 18:31:51 +0530

    namespace basic

---
+++ namespaceGS/Item.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Laptop;
+    const NUM = 10;
+    class Item1
+    {
+        function __construct()
+        {
+            echo "The Number of product is : 10001212";
+        }
+    }
+
+    function display()
+    {
+        echo "This is how you access or utilize the namespace";
+    }
+
+?>
---
+++ namespaceGS/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once('Item.php');
+echo Laptop\NUM;
+Laptop\display();
+echo "<br>";
+$obj = new Laptop\Item1();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Namespace</title>
+</head>
+<body>
+    <h5>Trying out namespace</h5>
+
+    <!-- echo Product\NUM; -->
+</body>
+</html>
